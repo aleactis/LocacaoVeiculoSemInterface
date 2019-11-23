0cfcc4f26a1c3328076aa38fe9e542065c09f11c
Alexandre Dionizio <aleactis@hotmail.com>
2019-11-23 16:00:20 -0300

    Finalizado

--- Interfaces/Services/AluguelServices.cs
+++ Interfaces/Services/AluguelServices.cs
@@ -10,6 +10,8 @@ namespace SemInterface.Services
         public double PrecoPorHora { get; set; }
         public double PrecoPorDia { get; set; }
 
+        //Declarar um objeto do tipo BrasilTaxaServices e instanciá-lo (Dependência). Não é ainda
+        //a melhor solução, pois uma melhor abordagem seria criar uma Interface.
         private BrasilTaxaServices _brasilTaxaService = new BrasilTaxaServices();
 
         public AluguelServices(double precoPorHora, double precoPorDia)
